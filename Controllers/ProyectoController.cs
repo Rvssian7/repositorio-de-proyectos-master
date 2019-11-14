21ed0c61659fe47c6367c48ad2ccc27723ab9808
Jose A <52551583+Rvssian7@users.noreply.github.com>
2019-11-13 22:11:29 -0500

    diseño

---
+++ Controllers/ProyectoController.cs
@@ -0,0 +1,152 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Repositorio.Models;
+
+namespace repositorio.Controllers
+{
+    public class ProyectoController : Controller
+    {
+        private readonly RepositorioContext _context;
+
+        public ProyectoController(RepositorioContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Proyecto
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Proyecto.ToListAsync());
+        }
+
+        // GET: Proyecto/Details/5
+        public async Task<IActionResult> Details(long? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var proyecto = await _context.Proyecto
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (proyecto == null)
+            {
+                return NotFound();
+            }
+
+            return View(proyecto);
+        }
+
+        // GET: Proyecto/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Proyecto/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Código,Nombre,Id")] Proyecto proyecto)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(proyecto);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(proyecto);
+        }
+
+        // GET: Proyecto/Edit/5
+        public async Task<IActionResult> Edit(long? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var proyecto = await _context.Proyecto.FindAsync(id);
+            if (proyecto == null)
+            {
+                return NotFound();
+            }
+            return View(proyecto);
+        }
+
+        // POST: Proyecto/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(long id, [Bind("Código,Nombre,Id")] Proyecto proyecto)
+        {
+            if (id != proyecto.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(proyecto);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ProyectoExists(proyecto.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(proyecto);
+        }
+
+        // GET: Proyecto/Delete/5
+        public async Task<IActionResult> Delete(long? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var proyecto = await _context.Proyecto
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (proyecto == null)
+            {
+                return NotFound();
+            }
+
+            return View(proyecto);
+        }
+
+        // POST: Proyecto/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(long id)
+        {
+            var proyecto = await _context.Proyecto.FindAsync(id);
+            _context.Proyecto.Remove(proyecto);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ProyectoExists(long id)
+        {
+            return _context.Proyecto.Any(e => e.Id == id);
+        }
+    }
+}
